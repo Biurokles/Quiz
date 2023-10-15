7dffc28360eae33c1a4ae0468a128f6200d6a5f1
roska <r.oskar.15@wp.pl>
2023-10-15 21:02:47 +0200

    Zadanie 2 wykonane. wszystko śmiga jak należy

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.quiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "correctAnswer";
+    public static final int REQUEST_CODE_PROMPT = 0;
+    public boolean answerWasShown;
     private int correctAnswers=0;
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
 
     private int correctAns = R.string.correct_answer;
@@ -29,30 +38,86 @@ public class MainActivity extends AppCompatActivity {
     };
     private int currentIndex = 0;
 
-    private void checkAsnwerCorrectness (boolean userAnswer)
-    {
+
+    private void checkAsnwerCorrectness (boolean userAnswer) {
         boolean correctAsnwer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        String result = "";
-        if(userAnswer == correctAsnwer){
-            resultMessageId=R.string.correct_answer;
-            correctAnswers+=1;
-            result = getString(resultMessageId)+" "+correctAnswers;
+        String result = null;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
+        } else {
+            result = "";
+            if (userAnswer == correctAsnwer) {
+                resultMessageId = R.string.correct_answer;
+                correctAnswers += 1;
+                result = getString(resultMessageId) + " " + correctAnswers;
+
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+                result = getString(resultMessageId);
+            }
 
         }
-        else{
-            resultMessageId = R.string.incorrect_answer;
-            result = getString(resultMessageId);
-        }
-        Toast.makeText(this,result , Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Start","No i się zaczęło");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Pause","No i się zawiesiło");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Resume","No i się ponowniło");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Destroy","No i się zniszczyło");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Stop","No i się zatrzymało");
+    }
+
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("QUIZ_TAG", "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        Log.d("QUIZ_TAG", "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null)
+        {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+
+        setContentView(R.layout.activity_main);
+        promptButton = findViewById(R.id.promptButton);
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
@@ -73,9 +138,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View v){
                 currentIndex = (currentIndex+1)%questions.length;
+                boolean answerWasShown = false;
                 setNextQuestion();
             }
         });
+        promptButton.setOnClickListener((v)->{
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+
+        });
+
+
+
+
+
+
         setNextQuestion();
     }
 
